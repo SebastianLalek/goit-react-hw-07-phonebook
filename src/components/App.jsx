0f8040ac1f10deb6bcb1ddad4d52821847b9433d
Sebastian Lalek <chemiczq@gmail.com>
2023-12-05 13:43:48 +0100

    move form events to Form component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,50 +3,11 @@ import Form from './form/form';
 import Section from './section/section';
 import Filter from './filter/filter';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { addContact, fetchContacts } from 'redux/operations';
-import { useEffect } from 'react';
-import Notiflix from 'notiflix';
-import { selectContacts } from 'redux/selectors';
-
 function Phonebook() {
-  const contacts = useSelector(selectContacts);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
-  const addNewContact = e => {
-    e.preventDefault();
-    const newName = e.target.name.value;
-    const newNumber = e.target.number.value;
-
-    if (
-      contacts.find(
-        contact => contact.name.toLowerCase() === newName.toLowerCase()
-      )
-    ) {
-      return Notiflix.Report.failure(
-        'Error',
-        `${newName} is already in contacts`,
-        'OK'
-      );
-    }
-
-    const newContact = {
-      name: newName,
-      number: newNumber,
-    };
-
-    dispatch(addContact(newContact));
-  };
-
   return (
     <div>
       <Section title="Phonebook">
-        <Form onSubmit={addNewContact} />
+        <Form />
       </Section>
       <Section title="Contacts">
         <ContactList>
--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,10 +1,49 @@
 import css from './form.module.css';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { selectContacts } from 'redux/selectors';
+import { addContact, fetchContacts } from 'redux/operations';
+
+import Notiflix from 'notiflix';
 import PropTypes from 'prop-types';
 
-export default function Form({ onSubmit, onChange }) {
+export default function Form() {
+  const contacts = useSelector(selectContacts);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  const addNewContact = e => {
+    e.preventDefault();
+    const newName = e.target.name.value;
+    const newNumber = e.target.number.value;
+
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === newName.toLowerCase()
+      )
+    ) {
+      return Notiflix.Report.failure(
+        'Error',
+        `${newName} is already in contacts`,
+        'OK'
+      );
+    }
+
+    const newContact = {
+      name: newName,
+      number: newNumber,
+    };
+
+    dispatch(addContact(newContact));
+  };
+
   return (
-    <form className={css.form} onSubmit={onSubmit}>
+    <form className={css.form} onSubmit={addNewContact}>
       <label className={css.label}>
         <p>Name</p>
         <input
@@ -12,7 +51,6 @@ export default function Form({ onSubmit, onChange }) {
           name="name"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          onChange={onChange}
         />
       </label>
       <label className={css.label}>
@@ -22,7 +60,6 @@ export default function Form({ onSubmit, onChange }) {
           name="number"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          onChange={onChange}
         />
       </label>
       <button className={css.button}>Add contact</button>
