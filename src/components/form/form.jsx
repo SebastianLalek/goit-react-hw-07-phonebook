7d8ec9db7a6099c68cb1ee8e6e242d5df4087204
Sebastian Lalek <chemiczq@gmail.com>
2023-12-05 13:27:07 +0100

    remove validation patterns

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -10,7 +10,6 @@ export default function Form({ onSubmit, onChange }) {
         <input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={onChange}
@@ -21,7 +20,6 @@ export default function Form({ onSubmit, onChange }) {
         <input
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           onChange={onChange}
